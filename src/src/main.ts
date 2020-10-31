80c45924b739716364b9fda046e1777ba0939fdb
adisakshya <adisakshya98@gmail.com>
2020-10-31 12:41:03 +0530

    serverless init

--- src/src/main.ts
+++ src/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { Context, Handler } from "aws-lambda";
+import { Server } from 'http';
+import { createServer, proxy } from 'aws-serverless-express';
+import * as Express from 'express';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+let cachedServer: Server;
+
+async function bootstrapServer(): Promise<Server> {
+    const expressApp = Express();
+    const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+    await nestApp.init();
+    return createServer(expressApp);
 }
-bootstrap();
+
+export const handler: Handler = async (event: any, context: Context) => {
+    if(!cachedServer) {
+        cachedServer = await bootstrapServer();
+    }
+    return proxy(cachedServer, event, context, 'PROMISE').promise;
+}

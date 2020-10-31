765628cf7d5d204d60641740ccb8997c7fb3dbba
adisakshya <adisakshya98@gmail.com>
2020-10-31 14:03:00 +0530

    removed app-controller & sls-http-event

--- src/src/main.ts
+++ src/src/main.ts
@@ -1,23 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import { Context, Handler } from "aws-lambda";
-import { Server } from 'http';
-import { createServer, proxy } from 'aws-serverless-express';
 import * as Express from 'express';
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+import { AppService } from './app.service';
 
-let cachedServer: Server;
-
-async function bootstrapServer(): Promise<Server> {
-    const expressApp = Express();
-    const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
-    await nestApp.init();
-    return createServer(expressApp);
+async function bootstrapServer(): Promise<INestApplication> {
+    const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(Express()));
+    return nestApp;
 }
 
 export const handler: Handler = async (event: any, context: Context) => {
-    if(!cachedServer) {
-        cachedServer = await bootstrapServer();
-    }
-    return proxy(cachedServer, event, context, 'PROMISE').promise;
+    const appServer = await bootstrapServer();
+    const appService = appServer.get(AppService);
+    const result = appService.getHello();
+    console.log('Execution Result:', result);
 }

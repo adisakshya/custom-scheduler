078f57dfc485b1673b778e7a057313a654c225cc
adisakshya <adisakshya98@gmail.com>
2020-11-01 20:24:08 +0530

    added query to get notifications for scheduling

--- src/src/scheduler/scheduler.service.ts
+++ src/src/scheduler/scheduler.service.ts
@@ -1,14 +1,20 @@
 import { Injectable, Logger } from '@nestjs/common';
+import { Equal } from 'typeorm';
 import { Notification } from '../database/entity/notification.entity';
 
 @Injectable()
 export class SchedulerService {
+  private readonly logger = new Logger("Scheduler Service");
   constructor() {}
-  
+
   async scheduleNotifications(): Promise<string> {
+    const currentTime = new Date().toUTCString();
+    const targetTime = new Date(new Date(currentTime).setSeconds(0, 0) + 1000 * 60);
+    this.logger.log(`Current Time is ${currentTime}`);
+    this.logger.log(`Getting notifications scheduled for ${targetTime}`);
     const [notifications, count] = await Notification.findAndCount({
       where: {
-        id: '1234'
+        deliverAt: Equal(targetTime)
       }
     });
     return `Scheduled ${count} notifications`;
